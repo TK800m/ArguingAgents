549c1d6c3254c406b82ae976fa392c2ceb2634f9
paulozk <p.m.ozkohen@student.rug.nl>
2018-10-17 16:17:52 +0100

    separated jupyter file and py script

---
+++ detect_and_score.py
@@ -0,0 +1,154 @@
+import gensim
+import keras
+import numpy as np
+from CMVscraper_script_delta2 import *
+import nltk
+from sklearn.metrics.pairwise import cosine_similarity
+
+def encode_post(sentences, topic, word2vec):
+    # given a list of sentences and the topic that the sentences
+    # are related to, encode the sentences using the word2vec model
+    # and compute the similarity of each word to the topic as
+    # an additional feature for each word
+    encoded_sentences = []
+    
+    # compute the average word vector of the topic, so that each word
+    # from the sentences can be compared to the topic
+    topic_words = topic.split()
+    topic_word_vectors = []
+    for word in topic_words:
+        # check if the word exists in the word2vec dictionary
+        if(word in word2vec):
+                word_vector = word2vec[word]
+         # else, map the word to a random, 300-dimensional vector
+        else:
+            word_vector = np.random.uniform(low = -0.01, high = 0.01, size = (300))
+        topic_word_vectors.append(word_vector)
+    topic_word_vectors = np.asarray(topic_word_vectors)
+    # the average topic vector is the average of all the words in it, along each f
+    # the 300 dimensions
+    avg_topic_vector = np.mean(topic_word_vectors, axis = 0)
+      
+    # for every sentence in the post...
+    for i in range(len(sentences)):
+        # get the words of the sentence by means of tokanization
+        # discarding punctuation marks
+        words = []
+        tokens = nltk.word_tokenize(sentences[i])
+        for token in tokens:
+            # only append actual words
+            if(token[0] not in ".,:;[](){}!?-_`'~\"^/1234567890"):
+                words.append(token)
+                
+        # store the word vectors into a sentence list
+        encoded_sentence = []
+                
+        # turn the words into word vectors
+        for word in words:
+            # check if the word exists in the word2vec dictionary
+            if(word in word2vec):
+                    word_vector = word2vec[word]
+             # else, map the word to a random, 300-dimensional vector
+            else:
+                word_vector = np.random.uniform(low = -0.01, high = 0.01, size = (300))
+        
+            # compute similarity between word and topic, then add this as the 
+            # 301-th feature
+            similarity = cosine_similarity([word_vector], [avg_topic_vector])
+            word_vector = np.append(word_vector, similarity) 
+            # add word to sentence list
+            encoded_sentence.append(word_vector)
+        encoded_sentence = np.asarray(encoded_sentence)
+        encoded_sentences.append(encoded_sentence)
+    # add encoded sentence to list of sentences      
+    encoded_sentences = np.asarray(encoded_sentences)
+        
+    return encoded_sentences
+        
+
+def detect_arguments(model, post, topic, word2vec):
+    # split text into sentences
+    post_sentences = post.split(".")
+    # encode the sentences
+    encoded_sentences = encode_post(post_sentences, topic, word2vec)
+     
+    # create lists to store the classified arguments and non-arguments
+    arguments = []
+    non_arguments = []
+    
+    #print(len(post_sentences), len(encoded_sentences))
+    #print(encoded_sentences[0])
+    #print(encoded_sentences[1])
+    
+    # feed sentences into LSTM and get prediction
+    for i in range(len(encoded_sentences)):
+        n_words = encoded_sentences[i].shape[0]
+        # skip empty sentences
+        if(n_words > 0):
+            n_features = encoded_sentences[i].shape[1]
+            prediction = model.predict(encoded_sentences[i].reshape(1, n_words, n_features), batch_size=1, verbose=0)
+            if(prediction > 0.5):
+                arguments.append((i, prediction, post_sentences[i]))
+            else:
+                non_arguments.append((i, prediction, post_sentences[i]))
+        else:
+             non_arguments.append((i, prediction, post_sentences[i]))
+            
+    return arguments, non_arguments, encoded_sentences
+
+def claim_or_evidence(model, arguments, sentences, encoded_sentences):
+    sentences = sentences.split(".")
+    # classify every argument as evidence or claim
+    classified_arguments = []
+    for arg in arguments:
+        # take the index from the tuple
+        idx = arg[0]
+        # take the encoded sentence corresponding to this
+        # argument
+        encoded_sentence = encoded_sentences[idx]
+       
+        n_words = encoded_sentence.shape[0]
+        n_features = encoded_sentence.shape[1]
+        score = model.predict(encoded_sentence.reshape(1, n_words, n_features), batch_size=1, verbose=0)
+        print(score[0])
+        # the LSTM is trained on discriminating between evidence sentences and non-evidence sentenves
+        # ASSUMPTION: if the argument is not an evidence sentence, it's a claim sentence
+        if(score >= 0.5):
+            classified_arguments.append((score[0], "EVIDENCE", sentences[idx]))
+        else:
+            classified_arguments.append((score[0], "CLAIM", sentences[idx]))
+    return classified_arguments
+
+def get_argument_quality(model, arguments, sentences, encoded_sentences):
+    sentences = sentences.split(".")
+    argument_scores = []
+    # score each argument
+    for arg in arguments:
+        # take the index from the tuple
+        idx = arg[0]
+        # take the encoded sentence corresponding to this
+        # argument
+        encoded_sentence = encoded_sentences[idx]
+        n_words = encoded_sentence.shape[0]
+        n_features = encoded_sentence.shape[1]
+        score = model.predict(encoded_sentence.reshape(1, n_words, n_features), batch_size=1, verbose=0)
+        argument_scores.append((score[0], sentences[idx]))
+    return argument_scores
+        
+def detect_and_score(detect_model, score_model, post, topic, word2vec):
+    # encode the sentences in the post and separate the arguments from the non-arguments
+    arguments, non_arguments, encoded_sentences = detect_arguments(detect_model, post, topic, word2vec)
+    # score the arguments
+    argument_scores = get_argument_quality(score_model, arguments, post, encoded_sentences)
+    return argument_scores
+
+def calculate_score(arguments):
+    score = 0
+    for arg in arguments:
+        score += arg[0]
+    return (score / len(arguments))[0]
+    
+    
+    
+    
+    
