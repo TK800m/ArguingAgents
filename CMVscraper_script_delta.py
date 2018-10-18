86e56c58e67690b744f34661f0d69f2c6f3a1a9f
TK800m <42999022+TK800m@users.noreply.github.com>
2018-10-18 23:03:27 +0200

    added kincaid

--- CMVscraper_script_delta.py
+++ CMVscraper_script_delta.py
@@ -192,3 +192,39 @@ def find_delta_txt(deltaNames, deltaComments, CommentText, commentName):
     return(deltaPost)
 
 #deltapost = find_delta_txt(deltaNames, deltaComments, CommentText, commentName)
+
+from textstat.textstat import textstat
+import numpy
+
+def flesch_kincaid_comm(text):
+    flesch_kincaid = []
+    for i in range(len(text)):
+        flesch_kincaid.append(textstat.flesch_kincaid_grade(text[i]))
+    return(flesch_kincaid)
+
+def flesch_ease_comm(text):
+    flesch_ease = []
+    for i in range(len(text)):
+        flesch_ease.append(textstat.flesch_reading_ease(text[i]))
+    return(flesch_ease)
+
+def flesch_kincaid_OP(text):
+    flesch_kincaid = textstat.flesch_kincaid_grade(text)
+    return(flesch_kincaid)
+
+def flesch_ease_OP(text):
+    flesch_ease = textstat.flesch_reading_ease(text)
+    return(flesch_ease)
+
+def similarEase(easyOP, easyComm, CommentText, commentName):
+    similar = []
+    saveCloseComm = []
+
+    for i in range(len(easyComm)):
+        test = abs(easyOP - easyComm[i])
+        if test <10:
+            similar =np.append(similar,easyComm[i])
+            saveCloseComm = np.append(saveCloseComm,i)
+           
+    return (similar, saveCloseComm)
+
--- CMVscraper_script_delta2.py
+++ CMVscraper_script_delta2.py
@@ -176,3 +176,38 @@ def getDeltaNames(deltalink, deltabot, CommentText):
 
 		
 #deltaNames, deltaComments = getDeltaNames(deltalink)
+
+from textstat.textstat import textstat
+import numpy
+
+def flesch_kincaid_comm(text):
+    flesch_kincaid = []
+    for i in range(len(text)):
+        flesch_kincaid.append(textstat.flesch_kincaid_grade(text[i]))
+    return(flesch_kincaid)
+
+def flesch_ease_comm(text):
+    flesch_ease = []
+    for i in range(len(text)):
+        flesch_ease.append(textstat.flesch_reading_ease(text[i]))
+    return(flesch_ease)
+
+def flesch_kincaid_OP(text):
+    flesch_kincaid = textstat.flesch_kincaid_grade(text)
+    return(flesch_kincaid)
+
+def flesch_ease_OP(text):
+    flesch_ease = textstat.flesch_reading_ease(text)
+    return(flesch_ease)
+
+def similarEase(easyOP, easyComm, CommentText, commentName):
+    similar = []
+    saveCloseComm = []
+
+    for i in range(len(easyComm)):
+        test = abs(easyOP - easyComm[i])
+        if test <10:
+            similar =np.append(similar,easyComm[i])
+            saveCloseComm = np.append(saveCloseComm,i)
+           
+    return (similar, saveCloseComm)
